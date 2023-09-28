12aec4e531e8389d31b18dbf6a0cbf870c50cb2c
ildizsigrai <ildi.zsigrai@gmail.com>
2023-09-28 09:11:15 +0200

    First commit

---
+++ src/Email.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private String email;
+	private int mailboxCapacity = 500;
+	private int defaultPasswordLength = 10;
+	private String alternateEmail;
+	private String companySuffix = "company.com";
+	
+	//Constructor to receive a first name and last name
+
+	public Email(String firstName, String lastName) {
+	    this.firstName = firstName;
+	    this.lastName = lastName;
+		
+		
+		//Call a method asking for a department and return the department
+		this.department = setDepartment();
+		
+		//Call method that returns a random password
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " + this.password);
+		
+		//Combine elements to generate email
+		email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
+	}
+
+	
+	//Ask for department
+	private String setDepartment() {
+		System.out.println("New user: " + firstName + " " + lastName + "\nDepartment codes: \n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) {
+			return "sales";
+		} else if (depChoice == 2) {
+			return "dev";
+		} else if (depChoice == 3) {
+			return "acct";
+		}else {
+			return "";
+		}
+	}
+	
+	//Generate a random password
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$%*&";
+		char[] password = new char[length];
+		for (int i = 0; i<length; i++) {
+			int rand = (int) (Math.random()* passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
+	}
+	
+	//Set mailbox capacity
+	public void setMailboxCapacity(int capacity) {
+		this.mailboxCapacity = capacity;
+	}
+	
+	//Set the alternate email
+	public void setAlternateEmail(String altEmail) {
+		this.alternateEmail = altEmail;
+	}
+	
+	//Change the password
+	public void changePassword(String password) {
+		this.password = password;
+	}
+	
+	public int getMailboxCapacity() { 
+		return mailboxCapacity;
+	}
+	public String getAlternateEmail() {
+		return alternateEmail;
+	}
+	public String getPassword() {
+		return password;
+	}
+	
+	public String showInfo() {
+		return "Display Name: " + firstName + " " + lastName +
+				"\nCompany Email: " + email + 
+				"\nMailbox Capacity: " + mailboxCapacity + "mb";
+	}
+}
---
+++ src/EmailApp.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EmailApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for their first name
+        System.out.print("Enter your first name: ");
+        String firstName = scanner.nextLine();
+
+        // Prompt the user for their last name
+        System.out.print("Enter your last name: ");
+        String lastName = scanner.nextLine();
+
+        // Create an Email instance with the provided first name and last name
+        Email em1 = new Email(firstName, lastName);
+
+        // Display the email information
+        System.out.println(em1.showInfo());
+
+        // Close the scanner when you're done using it to free up resources
+        scanner.close();
+    }
+}
